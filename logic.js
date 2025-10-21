d40a0056da673d254ab43c68201ef1dabb8c7b30
Riley Tan <xuanyun.tan@gmail.com>
2025-10-21 15:02:55 +0800

    Styled the special buttons and the grid
    Only special buttons are in a flex container. Grid does not seem to
    require any flex container.

---
+++ logic.js
@@ -0,0 +1,34 @@
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const subtract = (num1, num2) => {
+    return num1 - num2;
+}
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+}
+
+let firstNumber;
+let selectedOperator;
+let secondNumber;
+
+const operate = (num1, num2, operator) => {
+    if (operator === '+') {
+        return add(num1, num2)
+    }
+    else if (operator === '-') {
+        return subtract(num1, num2)
+    }
+    else if (operator === '*') {
+        return multiply(num1, num2)
+    }
+    else if (operator === '/') {
+        return divide(num1, num2)
+    }
+}
